560d12faa5056a085452b483ba184f07d0b7fc47
CHRNPS <6206021622178@fitm.kmutnb.ac.th>
2023-09-26 21:27:46 +0900

    edit

--- server.js
+++ server.js
@@ -3,34 +3,34 @@ const axios = require("axios");
 const app = express();
 const path = require("path");
 var BodyParser = require("body-parser");
-const base_url = "http://localhost:3000";
-// const base_url = "https://node52307-chern.proen.app.ruk-com.cloud/books";
+// const base_url = "http://localhost:3000";
+const base_url = "http://node52307-chern.proen.app.ruk-com.cloud/books";
 
 app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: false }));
 
-// app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get(base_url + "/books");
-    res.render("books", { books: response.data });
+      const response = await axios.get(base_url + '/books');
+      res.render("books", { books: response.data });
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Error");
+      console.error(err);
+      res.status(500).send('Error');
   }
 });
 
-app.get("/book/:id", async (req, res) => {
+app.get("/book/:id" , async (req, res) => {
   try {
-    const response = await axios.get(base_url + "/books/" + req.params.id);
-    res.render("book", { book: response.data });
+      const response = await axios.get(base_url + '/books/' + req.params.id);
+      res.render("book", { book: response.data });
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Error");
+      console.error(err);
+      res.status(500).send('Error');
   }
 });
 
@@ -40,46 +40,51 @@ app.get("/create", (req, res) => {
 
 app.post("/create", async (req, res) => {
   try {
-    const data = { title: req.body.title, author: req.body.author };
-    await axios.post(base_url + "/books", data);
-    res.redirect("/");
+      const data = { title: req.body.title, author: req.body.author };
+      await axios.post(base_url + '/books', data);
+      res.redirect("/");
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Error");
+      console.error(err);
+      res.status(500).send('Error');
   }
 });
 
 app.get("/update/:id", async (req, res) => {
   try {
-    const response = await axios.get(base_url + "/books/" + req.params.id);
-    res.render("update", { book: response.data });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error");
-  }
+      const response = await axios.get(
+          base_url + '/books/' + req.params.id);
+          res.render("update", {book: response.data});
+      } catch (err) {
+          console.error(err);
+          res.status(500).send('Error');
+      }
 });
 
 app.post("/update/:id", async (req, res) => {
   try {
-    const data = { title: req.body.title, author: req.body.author };
-    await axios.put(base_url + "/books/" + req.params.id, data);
-    res.redirect("/");
+      const data = { title : req.body.title, author: req.body.author };
+      await axios.put(base_url + '/books/' + req.params.id, data);
+      res.redirect("/");
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Error");
+      console.error(err);
+      res.status(500).send('Error');
   }
 });
 
 app.get("/delete/:id", async (req, res) => {
   try {
-    await axios.delete(base_url + "/books/" + req.params.id);
-    res.redirect("/");
+      await axios.delete(base_url + '/books/' + req.params.id);
+      res.redirect("/");
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Error");
+      console.error(err);
+      res.status(500).send('Error');
   }
 });
 
-app.listen(8080, () => {
-  console.log("Server start on port 8080");
+//app.listen(8080, () => {
+//    console.log('Server started on port 8080');
+//});
+
+app.listen(5500, () => {
+  console.log('Server started on port 5500');
 });
